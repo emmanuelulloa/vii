da89797b530faa3e36c29c5a6ddd4d1f08a87ccd
Emmanuel Ulloa <emmanuel.ulloa@popagency.com>
2015-06-01 18:42:47 -0600

    Added support for several frames

--- vii.js
+++ vii.js
@@ -40,10 +40,10 @@ vii = (function(){
 	};
 	//ease name shortcuts & special easings
 	ceaser.sine    = ceaser.easeOutSine;
-	ceaser.snap    = ceaser.easeInQuint;
-	ceaser.swing   = ceaser.easeOutBack;
 	ceaser.circ    = ceaser.easeOutCirc;
 	ceaser.expo    = ceaser.easeOutExpo;
+	ceaser.snap    = ceaser.easeInQuint;
+	ceaser.swing   = ceaser.easeOutBack;
 	ceaser.slowmo  = 'cubic-bezier(0,1,1,0)';
 	//check if a property is a configuration property or css property
 	function isCSS(prop){	
@@ -120,62 +120,86 @@ vii = (function(){
 	formatMap.d = formatMap.e = formatMap.m = formatMap.p = formatMap.op = formatMap.bd = formatMap.dy = formatMap.n = formatMap['none'];
 	//receives an object or spring and parse and reads its data
 	function parse(val){
-		var obj;
-		//check if string
+		var fr = [];
 		if(typeof val === 'string'){
-			obj = {};
-			var raw = val.split(' ');
-			for(var i = 0; i < raw.length; i++){
-				var nameValue = raw[i].split(':');
-				obj[nameValue[0]] = (nameValue[0] in formatMap)?formatMap[nameValue[0]](nameValue[1]):nameValue[1];
-			}
-		}else{
-			obj = val;
-		}
-		//create new object
-		var o = {};
-		for(var k in obj){
-			if(k in propMap){
-				o[propMap[k]] = obj[k];
+			//is string
+			if(val.indexOf('|') !== -1){
+				fr = val.split('|');
 			}else{
-				o[k] = obj[k];
+				fr = [val];
 			}
+		}else if(Object.prototype.toString.call(val) === '[object Object]'){
+			//is object
+			fr = [val];
+		}else if(Object.prototype.toString.call(val) === '[object Array]'){
+			//is array
+			fr = val;
 		}
-		//clean transforms
-		if('translateX' in o || 'translateY' in o || 'rotate' in o || 'skew' in o || 'scale' in o){
-			o['transform'] = '';
-			if('translateX' in o && 'translateY' in o){
-				o['transform'] += 'translate('+ o['translateX'] + ', ' + o['translateY'] + ') ' ;
-				delete o['translateX'];
-				delete o['translateY'];
-			}else{
-				if('translateX' in o){
-					o['transform'] += 'translateX(' + o['translateX'] + ') ' ;
-					delete o['translateX'];
+		var frames = [];
+		for(var i=0;i<fr.length;i++){
+			var obj;
+			//check if string
+			if(typeof fr[i] === 'string'){
+				obj = {};
+				var raw = fr[i].split(' ');
+				for(var j = 0; j < raw.length; j++){
+					var nameValue = raw[j].split(':');
+					obj[nameValue[0]] = (nameValue[0] in formatMap)?formatMap[nameValue[0]](nameValue[1]):nameValue[1];
 				}
-				if('translateY' in o){
-					o['transform'] += 'translateY(' + o['translateY'] + ') ';
+			}else{
+				obj = fr[i];
+			}
+			//create new object
+			var o = {};
+			for(var k in obj){
+				if(k in propMap){
+					o[propMap[k]] = obj[k];
+				}else{
+					o[k] = obj[k];
+				}
+			}
+			//clean transforms
+			if('translateX' in o || 'translateY' in o || 'rotate' in o || 'skew' in o || 'scale' in o){
+				o['transform'] = '';
+				if('translateX' in o && 'translateY' in o){
+					o['transform'] += 'translate('+ o['translateX'] + ', ' + o['translateY'] + ') ' ;
+					delete o['translateX'];
 					delete o['translateY'];
-				}				
+				}else{
+					if('translateX' in o){
+						o['transform'] += 'translateX(' + o['translateX'] + ') ' ;
+						delete o['translateX'];
+					}
+					if('translateY' in o){
+						o['transform'] += 'translateY(' + o['translateY'] + ') ';
+						delete o['translateY'];
+					}				
+				}
+				if('scale' in o){
+					o['transform'] += o['scale'];
+					delete o['scale'];
+				}
+				if('rotate' in o){
+					o['transform'] += o['rotate'];
+					delete o['rotate'];
+				}
+				if('skew' in o){
+					o['transform'] += o['skew'];
+					delete o['skew'];
+				}
 			}
-			if('scale' in o){
-				o['transform'] += o['scale'];
-				delete o['scale'];
+			//check if percentage is present
+			if(!o.percent){
+				o.percent = Math.round(i/(fr.length-1) * 100) + '%';
 			}
-			if('rotate' in o){
-				o['transform'] += o['rotate'];
-				delete o['rotate'];
-			}
-			if('skew' in o){
-				o['transform'] += o['skew'];
-				delete o['skew'];
-			}
+			frames.push(o);
 		}
-		return o;
+		return frames;
 	}
 	//receives an object the duration and the easing name
 	function getCSS(properties, duration, easing){
-		var prop = parse(properties),
+		var frames = parse(properties),
+			prop = frames[0],
 			useTransition = (prop.useTransition !== null)?prop.useTransition:false,
 			n = prop.name || 'myTween',
 			d = prop.duration || duration || '0.5',
@@ -203,27 +227,46 @@ vii = (function(){
 		delay += 's';
 		hacks += _t + '/*enable hardware acceleration*/' + _n;
 		hacks += _t + 'transform:translateZ(0)' + _e + _n;
-		hacks += _t + _w + 'transform:translateZ(0)' + _e + _n;
+		hacks += _t + _w + 'transform:translateZ(0)' + _e + _n + _n;
 		hacks += _t + '/*improve anti-alias*/'  + _n;
-		hacks += _t + 'box-shadow: 0 0 1px rgba(0, 0, 0, 0)' + _e + _n;
+		hacks += _t + 'box-shadow: 0 0 1px rgba(0, 0, 0, 0)' + _e + _n + _n;
 		hacks += _t + '/*font smoothing*/'  + _n;
 		hacks += _t + 'backface-visibility: hidden' + _e + _n;
 		hacks += _t + _w + 'backface-visibility: hidden' + _e + _n;
-		hacks += _t + '-moz-osx-font-smoothing: grayscale' + _e + _n;
+		hacks += _t + '-moz-osx-font-smoothing: grayscale' + _e + _n + _n;
+		//KEYFRAMES
 		if(!useTransition){
 			//keyframes
 			for(var px=0; px < prefix.length; px++){
-				t += '@' + prefix[px] + _k + n + _s + _o + _n;
-				t += _t + p + _s + _o + _n;
-				for(var k in prop){
-					if(isCSS(k)){
-						t += _t + _t + (k).replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase() + _s + _c + _s + prop[k] + _e + _n;
-					}else if(k === 'transform'){
-						t += _t + _t + prefix[px] + 'transform: ' + prop[k] + _e + _n;
+				var fQty = frames.length;
+				if(fQty == 1){
+					t += '@' + prefix[px] + _k + n + _s + _o + _n;
+					t += _t + p + _s + _o + _n;
+					for(var k in prop){
+						if(isCSS(k)){
+							t += _t + _t + (k).replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase() + _s + _c + _s + prop[k] + _e + _n;
+						}else if(k === 'transform'){
+							t += _t + _t + prefix[px] + 'transform: ' + prop[k] + _e + _n;
+						}
 					}
+					t += _t + _x + _n;
+					t += _x + _n;					
+				}else{
+					t += '@' + prefix[px] + _k + n + _s + _o + _n;
+					for(var i=0; i < fQty; i++){
+						p = (fQty == 2)?(i == 0)?'from':'to':frames[i].percent;
+						t += _t + p + _s + _o + _n;
+						for(var k in frames[i]){
+							if(isCSS(k)){
+								t += _t + _t + (k).replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase() + _s + _c + _s + prop[k] + _e + _n;
+							}else if(k === 'transform'){
+								t += _t + _t + prefix[px] + 'transform: ' + frames[i][k] + _e + _n;
+							}
+						}
+						t += _t + _x + _n;	
+					}
+					t += _x + _n;
 				}
-				t += _t + _x + _n;
-				t += _x + _n;
 			}
 			s += t;
 			//class
@@ -235,7 +278,7 @@ vii = (function(){
 			s += _t + _w + t;
 			s += _x + _n;		
 		}else{
-			//transition
+			//TRANSITION
 			s += _d + n + _s + _o + _n;
 			s += hacks;
 			t += 'transition:'
